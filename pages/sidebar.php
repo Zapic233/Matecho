5b204590e830f63d43187514d6228bd5e854b6db
KawaiiZapic <kawaiizapic@zapic.moe>
2024-03-25 17:41:44 +0800

    improve: optimize sidebar

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -6,13 +6,14 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 <mdui-navigation-drawer class="fixed! z-10000 top-64px! md:w-240px" close-on-overlay-click id="matecho-drawer">
     <mdui-list id="matecho-sidebar-list">
-        
-        <mdui-list-subheader>分类</mdui-list-subheader>
         <a href="/">
             <mdui-list-item <?php Matecho::activePage($this, "index"); ?> >
+                <mdui-icon-home slot="icon"></mdui-icon-home>
                 首页
             </mdui-list-item>
         </a>
+        <mdui-divider></mdui-divider>
+        <mdui-list-subheader>分类</mdui-list-subheader>
         <?php 
             $this->widget('Widget_Metas_Category_List')->to($category);
             /** @var \Widget\Metas\Category\Rows $category */
@@ -20,7 +21,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
                 if ($category->parent != 0) continue;
             ?>
                 <a href="<?php $category->permalink() ?>">
-                    <mdui-list-item <?php Matecho::activePage($this, "category", $category->mid); ?> >
+                    <mdui-list-item class="pl-42px" <?php Matecho::activePage($this, "category", $category->mid); ?> >
                         <?php echo $category->name ?>
                         <span class="text-xs opacity-60" slot="description"><?php echo $category->description ?></span>
                     </mdui-list-item>
