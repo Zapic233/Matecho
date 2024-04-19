a67b34f7a19fe8dccce8251e419b508e5aaa5af2
KawaiiZapic <kawaiizapic@zapic.moe>
2024-04-19 17:07:28 +0800

    fix: editor extend may not attached in slow network

--- src/pages/editor.ts
+++ src/pages/editor.ts
@@ -1,30 +1,29 @@
 import { initKaTeX, initPrism } from "./post";
 
 function init() {
-  const preview = document.querySelector<HTMLDivElement>("#wmd-preview");
-  if (!preview) return;
-  const { KaTeX, Prism } = window.__MATECHO_OPTIONS__;
-  const ob = new MutationObserver(() => {
-    if (window.getComputedStyle(preview).display === "none") return;
-    if (KaTeX) initKaTeX(preview);
-    if (Prism) {
-      preview.querySelectorAll("pre code[class]").forEach(el => {
-        el.classList.remove("focus");
-        el.parentElement?.classList.add(
-          "language-" + el.className.split(" ").filter(v => v != "focus")[0]
-        );
-      });
-      initPrism(preview);
-    }
-  });
-  ob.observe(preview, {
-    childList: true,
-    attributes: true
+  const int = setInterval(() => {
+    const preview = document.querySelector<HTMLDivElement>("#wmd-preview");
+    if (!preview) return;
+    clearInterval(int);
+    const { KaTeX, Prism } = window.__MATECHO_OPTIONS__;
+    const ob = new MutationObserver(() => {
+      if (window.getComputedStyle(preview).display === "none") return;
+      if (KaTeX) initKaTeX(preview);
+      if (Prism) {
+        preview.querySelectorAll("pre code[class]").forEach(el => {
+          el.classList.remove("focus");
+          el.parentElement?.classList.add(
+            "language-" + el.className.split(" ").filter(v => v != "focus")[0]
+          );
+        });
+        initPrism(preview);
+      }
+    });
+    ob.observe(preview, {
+      childList: true,
+      attributes: true
+    });
   });
 }
 
-if (document.readyState !== "loading") {
-  init();
-} else {
-  document.addEventListener("DOMContentLoaded", () => init());
-}
+init();
